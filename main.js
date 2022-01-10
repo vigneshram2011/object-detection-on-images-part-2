ba0f6666293486bceaa0c63cf5b67bdfcd3e0132
vigneshram2011 <78680381+vigneshram2011@users.noreply.github.com>
2022-01-11 10:26:16 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function bedroom_one() {
+    window.location = "bedroom_one.html";
+}
+
+function bedroom_two() {
+    window.location = "bedroom_two.html";
+}
+
+function bathroom() {
+    window.location = "bathroom.html";
+}
+
+function devices() {
+    window.location = "devices.html";
+}
+
+function garage() {
+    window.location = "garage.html";
+}
+
+function kitchen() {
+    window.location = "kitchen.html";
+}
